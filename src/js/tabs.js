93a162e25d9d0f5c4641686b9f032f5a60558e90
Albina <albinabist1993@mail.ru>
2021-09-20 12:40:01 +0300

    верстка "мои заявки"

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,4 +1,14 @@
-const tab = document.querySelector('.tabs-style');
-tab.addEventListener('click', function(){
-    tab.classList.toggle("tabs--active");
-})
+const tabsRequests = document.querySelector('.tabs_requests');
+const tabsEvents = document.querySelector('.tabs_events');
+
+if (tabsRequests.classList.contains('tabs--active')) {
+    tabsEvents.addEventListener('click', function(){
+        tabsEvents.classList.add("tabs--active");
+        tabsRequests.classList.remove("tabs--active");
+    });
+ } else if (tabsEvents.classList.contains('tabs--active')){
+    tabsRequests.addEventListener('click', function(){
+        tabsRequests.classList.add("tabs--active");
+        tabsRequests.classList.remove("tabs--active");
+    });
+ }
