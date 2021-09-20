ea620a9779bdfb150a9270dcb4c26151b85ab273
Albina <albinabist1993@mail.ru>
2021-09-20 15:02:42 +0300

    select

--- src/js/select.js
+++ src/js/select.js
@@ -2,6 +2,8 @@ const select = document.querySelector('.select__main-item');
 const selectList = document.querySelector('.select__items');
 const selectItem = document.querySelector('.select__item');
 
+select.innerHTML = selectItem.innerHTML;
+
 select.addEventListener('click', function(){
     if(selectList.classList.contains('select--close')){
         selectList.classList.remove("select--close");
--- src/js/tabs.js
+++ src/js/tabs.js
@@ -7,15 +7,15 @@ const tabsRequestsList = document.querySelector('.tabs-requests-list');
     tabsEvents.addEventListener('click', function(){
         tabsEvents.classList.add("tabs--active");
         tabsRequests.classList.remove("tabs--active");
-        tabsEventsList.classList.remove("tabs-list--close");
         tabsEventsList.classList.add("tabs-list--active");
+        tabsRequestsList.classList.remove("tabs-list--active");
     });
 
 
     tabsRequests.addEventListener('click', function(){
         tabsRequests.classList.add("tabs--active");
         tabsEvents.classList.remove("tabs--active");
-        tabsRequestsList.classList.remove("tabs-list--close");
         tabsRequestsList.classList.add("tabs-list--active");
+        tabsEventsList.classList.remove("tabs-list--active");
     });
  
