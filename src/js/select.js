abee6dc38403c16da20cd9f38dc7fc650d8d04c4
Albina <albinabist1993@mail.ru>
2021-09-20 12:18:35 +0300

    верстка "мои заявки"

---
+++ src/js/select.js
@@ -0,0 +1,4 @@
+const select = document.querySelector('.select__items');
+select.addEventListener('click', function(){
+    select.classList.toggle(".select--open");
+})
---
+++ src/js/tabs.js
@@ -0,0 +1,4 @@
+const tab = document.querySelector('.tabs-style');
+tab.addEventListener('click', function(){
+    tab.classList.toggle("tabs--active");
+})
