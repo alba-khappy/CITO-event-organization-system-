124928bc0e4a4fdd9de3f8387d2b1e807ac9de24
Albina <albinabist1993@mail.ru>
2021-09-22 17:55:28 +0300

    верстка календаря, попапа

--- src/js/warning.js
+++ src/js/warning.js
@@ -1,6 +1,11 @@
 const today = document.querySelector('.calendar__today');
 const popupWarning = document.querySelector('.warning');
+const closePopup = document.querySelector('.warning__button--close-popup');
 
 today.addEventListener('click', function(){
-    popupWarning.classList.toggle("warning--close");
+    popupWarning.classList.remove("warning--close");
+});
+
+closePopup.addEventListener('click', function(){
+    popupWarning.classList.add("warning--close");
 });
